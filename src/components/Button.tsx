c8742334781160be1afb8de81d0f1cf921fe5f44
thereallucas98 <lucas1998david@gmail.com>
2022-11-08 07:59:50 -0300

    update extend theme for buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,30 +1,15 @@
+import React from "react";
+
 import { Button as NativeBaseButton, IButtonProps, Text } from "native-base";
 
-type ButtonProps = IButtonProps & {
+type Props = IButtonProps & {
   title: string;
 };
 
-export function Button({ title, variant, ...rest }: ButtonProps) {
+export function Button({ title, ...rest }: Props) {
   return (
-    <NativeBaseButton
-      w="full"
-      h={14}
-      bg={variant === "outline" ? "transparent" : "green.700"}
-      borderWidth={variant === "outline" ? 1 : 0}
-      borderColor="green.500"
-      rounded="sm"
-      _pressed={{
-        bg: variant === "outline" ? "gray.500" : "green.500",
-      }}
-      {...rest}
-    >
-      <Text
-        color={variant === "outline" ? "green.500" : "white"}
-        fontFamily="heading"
-        fontSize="sm"
-      >
-        {title}
-      </Text>
+    <NativeBaseButton mb={4} {...rest}>
+      {title}
     </NativeBaseButton>
   );
 }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -35,4 +35,39 @@ export const THEME = extendTheme({
     14: 56,
     33: 148,
   },
+  components: {
+    Button: {
+      defaultProps: {
+        h: 14,
+        w: "full",
+        rounded: "sm",
+      },
+      variants: {
+        solid: {
+          bg: "green.700",
+          _pressed: {
+            bg: "green.500",
+          },
+          _text: {
+            fontSize: "md",
+            fontFamily: "heading",
+          },
+        },
+        outline: {
+          borderColor: `green.700`,
+          borderWidth: 1,
+          bg: "transparent",
+          rounded: "full",
+          _pressed: {
+            bg: "gray.500",
+          },
+          _text: {
+            color: "green.500",
+            fontSize: "md",
+            fontFamily: "heading",
+          },
+        },
+      },
+    },
+  },
 });
