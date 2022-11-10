8a68bbe109ef9d0b0617336f989d932a670b2153
thereallucas98 <lucas1998david@gmail.com>
2022-11-10 08:23:26 -0300

    fixing flatlist scrolling for android devices

--- src/screens/private/Home.tsx
+++ src/screens/private/Home.tsx
@@ -48,7 +48,7 @@ export function Home() {
         maxH={10}
       />
 
-      <VStack px={8}>
+      <VStack px={8} flex={1}>
         <HStack justifyContent="space-between" mb={5}>
           <Heading color="gray.200" fontSize="md">
             Exercícios
