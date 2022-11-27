2a790ee279feb80ae570c18277017f723f66d76b
thereallucas98 <lucas1998david@gmail.com>
2022-11-27 14:16:07 -0300

    services to backend

--- src/screens/public/SignUp.tsx
+++ src/screens/public/SignUp.tsx
@@ -1,7 +1,15 @@
 import { useNavigation } from "@react-navigation/native";
 
 import * as yup from "yup";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  useToast,
+} from "native-base";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 
@@ -11,6 +19,10 @@ import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
+import { api } from "@services/api";
+
+import { AppError } from "@utils/AppError";
+
 type FormDataProps = {
   name: string;
   email: string;
@@ -19,12 +31,12 @@ type FormDataProps = {
 };
 
 const signUpSchema = yup.object({
-  name: yup.string().required("Informe o nome."),
-  email: yup.string().required("Informe o e-mail").email("E-mail inválido."),
+  name: yup.string().required("Informe o nome"),
+  email: yup.string().required("Informe o e-mail").email("E-mail inválido"),
   password: yup
     .string()
     .required("Informe a senha")
-    .min(6, "A senha deve ter pelo menos 6 dígitos."),
+    .min(6, "A senha deve ter pelo menos 6 dígitos"),
   password_confirm: yup
     .string()
     .required("Confirme a senha")
@@ -42,17 +54,28 @@ export function SignUp() {
 
   const navigation = useNavigation();
 
+  const toast = useToast();
+
   function handleGoBackToLogin() {
     navigation.goBack();
   }
 
-  function handleSignUp({
-    name,
-    email,
-    password,
-    password_confirm,
-  }: FormDataProps) {
-    console.log({ name, email, password, password_confirm });
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      const response = await api.post("/users", { name, email, password });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta. Tente novamente mais tarde";
+
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
   }
 
   return (
