50d2463db3617dfe64c974339f7350d253ff864a
thereallucas98 <lucas1998david@gmail.com>
2022-12-13 19:41:53 -0300

    updating profile

--- src/screens/public/SignIn.tsx
+++ src/screens/public/SignIn.tsx
@@ -96,7 +96,6 @@ export function SignIn() {
 
         <Center my={24}>
           <LogoSvg />
-
           <Text color="gray.100" fontSize="sm">
             Treine sua mente e o seu corpo
           </Text>
