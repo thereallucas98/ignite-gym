83da90cbb316bc5a910b63f68e1594105b66110e
thereallucas98 <lucas1998david@gmail.com>
2022-12-15 19:45:34 -0300

    verifying if token is valid

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,27 +6,36 @@ type SignOut = () => void;
 
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
-}
+};
 
 const api = axios.create({
   baseURL: "http://192.168.27.106:3333",
 }) as APIInstanceProps;
 
-api.registerInterceptTokenManager = singOut => {
+api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
     (response) => response,
-    (error) => {
-      if (error.response && error.response.data) {
-        return Promise.reject(new AppError(error.response.data.message));
+    (requestError) => {
+      if (requestError.response?.status === 401) {
+        if (
+          requestError.response.data?.message === "token.expired" ||
+          requestError.response.data?.message === "token.invalid"
+        ) {
+          signOut();
+        }
+      }
+
+      if (requestError.response && requestError.response.data) {
+        return Promise.reject(new AppError(requestError.response.data.message));
       } else {
-        return Promise.reject(error)
+        return Promise.reject(requestError);
       }
     }
   );
 
   return () => {
     api.interceptors.response.eject(interceptTokenManager);
-  }
-}
+  };
+};
 
 export { api };
